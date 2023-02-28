dce0638e479968bd24ca9d2a741df1dc6144b03d
Jian Lu <jason.lu1932@gmail.com>
2023-02-28 00:04:17 -0500

    built an instructions component to list out the steps of setting up the app and decreased font size for web view

--- pages/components/(instructions).tsx
+++ pages/components/(instructions).tsx
@@ -1,9 +1,20 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../../styles/Home.module.css';
 
-const Instructions = () => {
+const Instructions = (props) => {
     return (
-
+        <div className={styles.instructionsContainer}>
+            <div className={styles.stepNumber}>
+                {props.stepNumber}
+            </div>
+            <p className={styles.step}>{props.instructions} 
+                {
+                    props.link ? <Link href={props.link} className={styles.stepLink} style={{ textDecoration: 'underline' }}>here</Link> : undefined
+                }
+                .
+            </p>
+        </div>
     )
 }
 
